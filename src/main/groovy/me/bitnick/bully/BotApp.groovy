9473b46daca0c444791ecd751d026f35471bba77
nstrydom <nstrydom@gmail.com>
2021-09-15 22:58:17 -0500

    [Dev] Removed `main()` code, return

--- src/main/groovy/me/bitnick/bully/BotApp.groovy
+++ src/main/groovy/me/bitnick/bully/BotApp.groovy
@@ -1,16 +1,14 @@
 package me.bitnick.bully
 
+import edu.self.kraken.api.KrakenApi
+import groovy.json.JsonSlurper
 import groovy.util.logging.Slf4j
-import me.bitnick.bully.broker.SampleClass
+import me.bitnick.bully.exchange.Krak
+
 
 @Slf4j
 class BotApp {
     static void main(String[] args) {
-        var test = new SampleClass()
-        test.websocketTickDataExample()
 
-        while (true) {
-            Thread.sleep(850)
-        }
     }
 }
